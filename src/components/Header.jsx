99796f4e68cbff32b0de2b52be6ef35ef515ee17
yeffry <yeffry45@gmail.com>
2021-12-14 16:48:10 -0500

    add Header component skeleton

---
+++ src/components/Header.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import '../css/header.css'
+
+const Header = () => {
+  return (
+    <header>
+      <h1>Fruit Memory Game</h1>
+      <div>
+        <span>Score</span>
+        <span>Highest Score</span>
+      </div>
+    </header>
+  )
+}
+
+export default Header
