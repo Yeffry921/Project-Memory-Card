882e7664d715426a09fb94ae405769fe06a005d4
yeffry <yeffry45@gmail.com>
2021-12-14 16:47:06 -0500

    add fruit data file

---
+++ src/data.js
@@ -0,0 +1,23 @@
+import apple from './imgs/apple.png'
+import grape from './imgs/grape.png'
+import lemon from './imgs/lemon.png'
+
+const fruitData = [
+  {
+    id: 1,
+    name: 'Apple',
+    src: apple,
+  },
+  {
+    id: 2,
+    name: 'Grape',
+    src: grape,
+  },
+  {
+    id: 3,
+    name: 'Lemon',
+    src: lemon,
+  }
+]
+
+export default fruitData
