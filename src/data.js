26c6843e0463d57d93a27270e8fc7971ef2062bf
yeffry <yeffry45@gmail.com>
2021-12-15 12:45:02 -0500

    add data + images

--- src/data.js
+++ src/data.js
@@ -1,6 +1,10 @@
 import apple from './imgs/apple.png'
 import grape from './imgs/grape.png'
 import lemon from './imgs/lemon.png'
+import orange from './imgs/orange.png'
+import pineapple from './imgs/pineapple.png'
+import strawberry from './imgs/strawberry.png'
+import watermelon from './imgs/watermelon.png'
 
 const fruitData = [
   {
@@ -17,6 +21,25 @@ const fruitData = [
     id: 3,
     name: 'Lemon',
     src: lemon,
+  }, 
+  {
+    id: 4,
+    name: 'Orange',
+    src: orange,
+  }, {
+    id: 5,
+    name: 'Pineapple',
+    src: pineapple
+  },
+  {
+    id: 6,
+    name: 'Strawberry',
+    src: strawberry
+  }, 
+  {
+    id: 7,
+    name: 'Watermelon',
+    src: watermelon
   }
 ]
 
