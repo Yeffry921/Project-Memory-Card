a2f406c06670e73bc8cab2897d971fe64b883544
yeffry <yeffry45@gmail.com>
2021-12-15 10:36:50 -0500

    add description wrapper + add class names to containers

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,10 +4,18 @@ import '../css/header.css'
 const Header = () => {
   return (
     <header>
-      <h1>Fruit Memory Game</h1>
-      <div>
-        <span>Score</span>
-        <span>Highest Score</span>
+      <div className="header__title-wrapper">
+        <h1>Fruit Memory Game</h1>
+      </div>
+      <div className="header__scores-wrapper">
+        <span>Score: {0}</span>
+        <span>Highest Score: {0}</span>
+      </div>
+      <div className="header__description-wrapper">
+        <p>You are presented with multiple images of fruits.
+          You cannot click on any image more than once or you lose.
+          Good Luck
+        </p>
       </div>
     </header>
   )
