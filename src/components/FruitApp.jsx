25298c61073fc771efc3aa475d299748a9add33a
yeffry <yeffry45@gmail.com>
2021-12-15 12:06:18 -0500

    create FruitApp component to house the game logic

---
+++ src/components/FruitApp.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+import Header from "./Header"
+import GameBody from "./GameBody";
+
+const FruitApp = () => {
+  return(
+    <React.Fragment>
+      <Header/>
+      <GameBody/>
+    </React.Fragment>
+  )
+}
+
+export default FruitApp
