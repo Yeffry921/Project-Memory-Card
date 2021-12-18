97a0260412930b0af74146e17a251dff5cdd5869
yeffry <yeffry45@gmail.com>
2021-12-18 16:42:45 -0500

    add useEffect hook for shuffling data

--- src/components/FruitApp.jsx
+++ src/components/FruitApp.jsx
@@ -2,15 +2,27 @@ import React, { useState, useEffect } from "react";
 
 import Header from "./Header"
 import GameBody from "./GameBody";
+import data from '../data'
 
 const FruitApp = () => {
 
   const [score, setScore] = useState(0)
   const [highScore, setHighScore] = useState(0)
-  const [fruits, setFruits] = useState([])
+  const [fruitsClicked, setFruits] = useState([])
+  const [fruitData, setFruitData] = useState(data)
+
+  useEffect(()=> {
+    const shuffleArray = (array) => {
+      return array.sort(() => Math.random() - 0.5);
+    }
+
+    const shuffledFruits = shuffleArray(data)
+    setFruitData(shuffledFruits)
+
+  }, [fruitsClicked])
 
   const clickedFruits = (event, id) => {
-    if(fruits.includes(id)) {
+    if(fruitsClicked.includes(id)) {
       // If you click on 
       resetScore()
       placeHighScore()
@@ -39,7 +51,6 @@ const FruitApp = () => {
     }
   }
 
-  console.log(fruits)
   return(
     <React.Fragment>
       <Header gameScoreData={score} highScoreData={highScore}/>
@@ -48,6 +59,7 @@ const FruitApp = () => {
         onAddToScore={addToScore}
         onResetScore={resetScore}
         onPlaceHighScore={placeHighScore}
+        fruitDataState={fruitData}
       />
     </React.Fragment>
   )
