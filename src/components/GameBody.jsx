c54fcb1677ca4fcf5e847455ba90fe49125f7e18
yeffry <yeffry45@gmail.com>
2021-12-18 12:59:47 -0500

    add onclick prop event to component

--- src/components/GameBody.jsx
+++ src/components/GameBody.jsx
@@ -1,12 +1,17 @@
+import React, { useEffect, useState } from 'react'
+
 import data from '../data'
 import '../css/gamebody.css'
+import '../css/mediaQuery.css'
 
-const GameBody = () => {
+
+const GameBody = ({ onClickedFruits }) => {
+
   return (
     <div className='game__card-wrapper'>
       {data.map((fruit) => {
         return (
-          <div className='game__card' key={fruit.id}>
+          <div className='game__card' key={fruit.id} onClick={(e) => onClickedFruits(e, fruit.id)}>
             <img src={fruit.src} alt={fruit.name} />
             <p>{fruit.name}</p>
           </div>
