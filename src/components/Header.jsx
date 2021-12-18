6a4784fa836087a284edca99f5014e4fa1f1fcff
yeffry <yeffry45@gmail.com>
2021-12-18 12:58:22 -0500

    fix prop data names for rendering

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,15 +1,15 @@
 import React from "react";
 import '../css/header.css'
 
-const Header = () => {
+const Header = ({ gameScoreData, highScoreData }) => {
   return (
     <header>
       <div className="header__title-wrapper">
         <h1>Fruit Memory Game</h1>
       </div>
       <div className="header__scores-wrapper">
-        <span>Score: {0}</span>
-        <span>Highest Score: {0}</span>
+        <span>Score: {gameScoreData}</span>
+        <span>Highest Score: {highScoreData}</span>
       </div>
       <div className="header__description-wrapper">
         <p>You are presented with multiple images of fruits.
