9d435d612af8f8af0319c3aa7ebe37608e1aa448
yeffry <yeffry45@gmail.com>
2021-12-18 12:57:49 -0500

    add working logic for basic game to be played

--- src/components/FruitApp.jsx
+++ src/components/FruitApp.jsx
@@ -1,13 +1,52 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
 import Header from "./Header"
 import GameBody from "./GameBody";
 
 const FruitApp = () => {
+
+  const [score, setScore] = useState(0)
+  const [highScore, setHighScore] = useState(0)
+  const [fruits, setFruits] = useState([])
+
+  const clickedFruits = (event, id) => {
+    if(fruits.includes(id)) {
+      // If you click on 
+      resetScore()
+      placeHighScore()
+      setFruits([])
+      return;
+    }
+
+    // Game continues
+    setFruits((prevState) => {
+      return [...prevState, id]
+    })
+    addToScore()
+  }
+
+  const addToScore = () => {
+    setScore(score + 1)
+  }
+
+  const resetScore = () => {
+    setScore(0)
+  }
+
+  const placeHighScore = () => {
+    setHighScore(score)
+  }
+
+  console.log(fruits)
   return(
     <React.Fragment>
-      <Header/>
-      <GameBody/>
+      <Header gameScoreData={score} highScoreData={highScore}/>
+      <GameBody 
+        onClickedFruits={clickedFruits}
+        onAddToScore={addToScore}
+        onResetScore={resetScore}
+        onPlaceHighScore={placeHighScore}
+      />
     </React.Fragment>
   )
 }
